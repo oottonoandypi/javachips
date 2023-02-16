6a3f4615d45902edc50a887b8366818f016d658c
yunyisilva <yunyid8+github@gmail.com>
2023-02-16 13:20:23 -0700

    Added Bit1Counter.java.

---
+++ Bit1Counter.java
@@ -0,0 +1,27 @@
+package practice;
+import java.util.Scanner;
+
+public class Bit1Counter {
+	public int countBit1(int n) {
+		int count=0;
+        for(int i=0; i<32; i++){
+            if((n&1)==1) count++;
+            n>>=1;
+        }
+        return count;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Please enter the integer that you'd like to count 1's.\n(Note: Bit1Counter counts 32-bit integer.)");
+		Scanner scan = new Scanner(System.in);
+		Bit1Counter counter = new Bit1Counter();
+		try {
+			int count = counter.countBit1(scan.nextInt());
+			scan.close();
+			System.out.println("Result: "+count+" bits of 1's.");
+		}catch(Exception e) {
+			System.out.println("ERROR:: "+e);
+		}
+		
+	}
+}
