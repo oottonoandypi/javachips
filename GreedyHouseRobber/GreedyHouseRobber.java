36f3662f8996c1932c3429bc0564735bc55be53a
yunyisilva <yunyid8+github@gmail.com>
2023-03-09 17:09:08 -0700

    Added maxAmount_circularHouses function.

--- GreedyHouseRobber/GreedyHouseRobber.java
+++ GreedyHouseRobber/GreedyHouseRobber.java
@@ -4,6 +4,26 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class GreedyHouseRobber {
+	public int maxAmount_circularHouses(int[] nums) {
+        if(nums.length==1) return nums[0];
+        if(nums.length<3) return Math.max(nums[0], nums[1]);
+        
+        int houses = nums.length;
+        int[] noReachEnd=new int[houses];
+        noReachEnd[houses-1]=0;
+        noReachEnd[houses-2]=nums[houses-2];
+        noReachEnd[houses-3]=nums[houses-3];
+        nums[houses-3]+=nums[houses-1];
+        
+        for(int i=houses-4; i>=0; i--){
+            noReachEnd[i]=nums[i]+Math.max(noReachEnd[i+2], noReachEnd[i+3]);
+            nums[i]+=Math.max(nums[i+2], nums[i+3]);
+        }
+        
+        return Math.max(Math.max(nums[1], nums[2]), Math.max(noReachEnd[0], noReachEnd[1]));
+       
+    }
+	
 	public int maxAmount(int[] nums) {
 		// robbing adjacent houses will alert the police.
 		// maxAmount that could steal without alerting the police.
