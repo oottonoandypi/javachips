65057183535de80de3b8132d2082e3be3de54949
yunyisilva <yunyid8+github@gmail.com>
2023-03-31 14:34:10 -0700

    Renamed BinaryTree.java -> BinaryTreeOps.java.

--- BinaryTreeOps.java
+++ BinaryTreeOps.java
@@ -3,8 +3,8 @@ package javachips;
 import java.util.List;
 import java.util.ArrayList;
 
-public class BinaryTree {
-	class TreeNode {
+public class BinaryTreeOps {
+	private class TreeNode {
 		int val;
 		TreeNode left;
 		TreeNode right;
@@ -17,17 +17,17 @@ public class BinaryTree {
 		}
 	}
 	
-	int kth = -1;
-    int K=1;
+	private static int kth = -1; // local variable for kthSmallest
+	private static int K=1; // local variable for kthSmallest
     // find the kth smallest integer from the Tree
     // DFS without using arraylist to store values in order.
-	public int kthSmallest(TreeNode root, int k) {
-        K=k;
+	public static int kthSmallest(TreeNode root, int k) {
+		K=k;
         searchKth(root);
         return kth;
     }
 	// helper function for kthSmallest
-    private void searchKth(TreeNode root){
+    private static void searchKth(TreeNode root){
         if(root==null) return;
         if(kth==-1){
             searchKth(root.left);
@@ -42,13 +42,13 @@ public class BinaryTree {
     
     // find the kth smallest integer from the Tree
     // DFS without using arraylist to store values in order.
-    public int kthSmallest_arraylist(TreeNode root, int k) {
+    public static int kthSmallest_arraylist(TreeNode root, int k) {
         List<Integer> inorder=new ArrayList<Integer>();
         treeToArray(root, inorder);
         return inorder.get(k-1);
     }
     // helper function for kthSmallest_arraylist
-    private void treeToArray(TreeNode root, List<Integer> inorder){
+    private static void treeToArray(TreeNode root, List<Integer> inorder){
         if(root==null) return;
         
         treeToArray(root.left, inorder);
@@ -56,7 +56,7 @@ public class BinaryTree {
         treeToArray(root.right, inorder);
     }
 	
-	public TreeNode invertTree(TreeNode root) {
+	public static TreeNode invertTree(TreeNode root) {
         if(root==null) return root;
         TreeNode right=invertTree(root.left);
         root.left=invertTree(root.right);
