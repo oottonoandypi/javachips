56d8e784fd767f1d32daf58894c5afdce4b890eb
yunyisilva <yunyid8+github@gmail.com>
2023-05-30 15:37:17 -0700

    Added a findWords_I method to ArrayOfStringOps.java.

--- ArrayOfStringOps.java
+++ ArrayOfStringOps.java
@@ -1,11 +1,57 @@
 package javachips;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ArrayOfStringOps {
+	// ---------------------------------------------------------
+	// Using American keyboard
+	// Return the words from array words that can be typed using only 1 of the 3 rows on the keyboard
+	public static String[] findWords_I(String[] words) {
+		// runtime O(n); memory usage O(1)
+		
+        int[][] rows=new int[3][26];
+        
+        String row1Chars="qwertyuiop";
+        for(int i=0; i<row1Chars.length(); i++){
+            rows[0][row1Chars.charAt(i)-'a']=1;
+        }
+        String row2Chars="asdfghjkl";
+        for(int i=0; i<row2Chars.length(); i++){
+            rows[1][row2Chars.charAt(i)-'a']=1;
+        }
+        String row3Chars="zxcvbnm";
+        for(int i=0; i<row3Chars.length(); i++){
+            rows[2][row3Chars.charAt(i)-'a']=1;
+        }
+        
+        int row;
+        List<String> res= new ArrayList<String>();
+    
+        for(String word: words){
+            for(row=0; row<3; row++){
+                if((word.charAt(0)<='Z' && rows[row][word.charAt(0)+32-'a']==1) || (word.charAt(0)>'Z' && rows[row][word.charAt(0)-'a']==1)) break;
+            }
+            // System.out.println("Row: "+row);
+            for(int i=0; i<word.length(); i++){
+                if((word.charAt(i)<='Z' && rows[row][word.charAt(i)+32-'a']!=1) || (word.charAt(i)>'Z' && rows[row][word.charAt(i)-'a']!=1)) break;
+                if (i==word.length()-1) res.add(word);
+                
+            }
+        }
+        
+        String[] resToArray=new String[res.size()];
+        for(int i=0; i<resToArray.length; i++){
+            resToArray[i]=res.get(i);
+        }
+        
+        return resToArray;
+    }
+	
 	// Find the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters
 	// If no such two words exist, return 0;
-	public int maxProduct_bitCompare(String[] words) {
+	public static int maxProduct_bitCompare(String[] words) {
 		// runtime O(n^2+n*k); additional memory n+26n
         int[] wordsInBits=new int[words.length];
         int res=0;
@@ -31,7 +77,7 @@ public class ArrayOfStringOps {
         return res;
     }
 	
-	public int maxProduct_naive(String[] words) {
+	public static int maxProduct_naive(String[] words) {
 		// runtime O(2n^2*k); additional memory 26*n*2
         int res=0;
         int[][] isLetterInWord=new int[26][words.length];
