c24ace04fab0f50d391632e9d1674cb56ca96b77
yunyisilva <yunyid8+github@gmail.com>
2023-06-01 10:11:58 -0700

    Added 2 implementations of findMode_ method in BinarySearchTreeOps.java.

--- BinarySearchTreeOps.java
+++ BinarySearchTreeOps.java
@@ -1,5 +1,7 @@
 package javachips;
 
+import java.util.ArrayList;
+
 public class BinarySearchTreeOps {
 	private class TreeNode {
 		int val;
@@ -13,6 +15,101 @@ public class BinarySearchTreeOps {
 			this.right = right;
 		}
 	}
+	
+	// ---------------------------------------------------------
+	// Find Mode: The value(s) that appear most frequent in a BST
+	private static int mostFreq=0;
+    private static int count=0;
+    private static TreeNode prev=null;
+    private static ArrayList<Integer> modes=new ArrayList<Integer>();
+    
+    // in-order DFS approach
+    // runtime O(n); Memory usage O(1)
+    public static int[] findMode_InOrder(TreeNode root) {
+    	searchModes_InOrder(root);
+        
+        if(prev!=null && count>=mostFreq){
+            if(count>mostFreq) {
+                mostFreq=count;
+                modes.clear();
+            }
+            modes.add(prev.val);
+        }
+        
+        int[] res=new int[modes.size()];
+        for(int i=0; i<modes.size(); i++){
+            res[i]=modes.get(i);
+        }
+        return res;
+    }
+    
+    private static void searchModes_InOrder(TreeNode node){
+        if(node==null) return;
+        searchModes_InOrder(node.left);
+        
+        if(prev!=null){
+            if(prev.val==node.val) count++;
+            else{
+                if(count>=mostFreq){
+                    if(count>mostFreq) {
+                        mostFreq=count;
+                        modes.clear();
+                    }
+                    modes.add(prev.val);
+                }
+                prev=node;
+                count=1;
+            }
+        }else{
+            prev=node;
+            count=1;
+        }
+        
+        searchModes_InOrder(node.right);
+    }
+	
+    
+    // post-order DFS apprach
+    // runtime O(n); memory usage O(n)
+    public static int[] findMode_PostOrder(TreeNode root) {
+    	searchModes_PostOrder(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        
+        int[] res=new int[modes.size()];
+        for(int i=0; i<modes.size(); i++){
+            res[i]=modes.get(i);
+        }
+        return res;
+    }
+    
+    private static int[] searchModes_PostOrder(TreeNode node, int smallest, int largest){
+        if(node==null) return new int[2];
+        
+        int[] leftModes=searchModes_PostOrder(node.left, smallest, node.val);
+        int[] rightModes=searchModes_PostOrder(node.right, node.val, largest);
+        
+        int[] counts=new int[2];
+        if(node.val>smallest && node.val<largest){
+            int count=1;
+            count+=leftModes[1];
+            count+=rightModes[0];
+            if(count>=mostFreq) {
+                if(count>mostFreq){
+                    modes.clear();
+                    mostFreq=count;
+                }
+                modes.add(node.val);
+            }
+        }
+        counts[0]=leftModes[0];
+        counts[1]=rightModes[1];
+        
+        if(node.val==smallest) counts[0]+=rightModes[0]+1;
+        if(node.val==largest) counts[1]+=leftModes[1]+1;
+        
+        return counts;
+    }
+    
+	// ---------------------------------------------------------
 	
 	public static TreeNode lowestCommonAncestor_iter(TreeNode root, TreeNode p, TreeNode q) {
         // binery search; runtime O(lgn) iteration approach using while loop; O(1) additional memory usage
@@ -34,6 +131,8 @@ public class BinarySearchTreeOps {
         
         return findLCA(root, min, max);
     }
+	
+	// ---------------------------------------------------------
     
     private static TreeNode findLCA(TreeNode root, int min, int max){
         if(root==null) return null;
