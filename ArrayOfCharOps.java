a8f3ed745dd5ab32191c63df56ec76f0ae5bd6ca
yunyisilva <yunyid8+github@gmail.com>
2023-06-08 18:53:06 -0700

    Added a smallestLargerLetter method to ArrayOfCharOps.java.

--- ArrayOfCharOps.java
+++ ArrayOfCharOps.java
@@ -1,6 +1,27 @@
 package javachips;
 
 public class ArrayOfCharOps {
+	// ---------------------------------------------------------
+	// Given a non-decreasing array of characters and a character target
+	// Return the smallest letter in the array that is larger than target
+	// If no such letter, return the first letter in the array
+	public char smallestLargerLetter(char[] letters, char target) {
+		// runtime O(lgn) memory O(1)
+        int l=0;
+        int r=letters.length-1;
+        if(letters[r]<=target) return letters[0];
+        int m;
+        
+        while(l<=r){
+            m=l+(r-l)/2;
+            if(letters[m]<=target) l=m+1;
+            else r=m-1;
+        }
+        
+        return letters[l];
+    }
+	
+	// ---------------------------------------------------------
 	public void reverseString(char[] s) {
         int len=s.length;
         for(int i=0; i<=(len-1)/2; i++){
