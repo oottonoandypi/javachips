da34df89edf83382048ff0b0b499eed8b19aef86
yunyisilva <yunyid8+github@gmail.com>
2023-06-16 12:49:03 -0700

    Added 3 implementations of findConcatenatedSubstrings_* method in ArrayOfStringOps.java.

--- ArrayOfStringOps.java
+++ ArrayOfStringOps.java
@@ -3,8 +3,172 @@ package javachips;
 import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Arrays;
 
 public class ArrayOfStringOps {
+	// ---------------------------------------------------------
+	// Find Substring with Concatenation of All Words
+	// Inputs: a string s and an array of strings words. 
+	// All the strings of words are of the same length.
+	// Return: the starting indices of all the concatenated substrings in s.
+	// 
+	// Note: A concatenated substring in s is a substring that contains all the strings of any permutation of words concatenated.
+	// EX. if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings
+	
+	// Approach #3: same idea as _binarysearch below, but use hashmap instead so searching is faster than lgn
+	public List<Integer> findConcatenatedSubstrings_hashmap(String s, String[] words) {
+        int wordsLen=words.length;
+        int wLen=words[0].length();
+        int substrLen=wordsLen*wLen;
+        HashMap<String, Integer> wordsAtIndex=new HashMap<String, Integer>();
+        int[] wordsCounts=new int[wordsLen];
+        int uniqueWords=-1;
+        
+        for(String word: words){
+            if(!wordsAtIndex.containsKey(word)) {
+                uniqueWords++;
+                wordsAtIndex.put(word, uniqueWords);
+                wordsCounts[uniqueWords]++;
+            }else {
+                wordsCounts[wordsAtIndex.get(word)]++;
+            }
+        }
+        
+        int[] counts;
+        List<Integer> res=new ArrayList<Integer>();
+        String substr;
+        int j;
+        
+        for(int i=0; i+substrLen<=s.length(); i++){
+            counts=new int[uniqueWords+1];
+            j=i;
+            while(j<=i+substrLen-wLen){
+                substr=s.substring(j,j+wLen);
+                if(!wordsAtIndex.containsKey(substr) || counts[wordsAtIndex.get(substr)]>=wordsCounts[wordsAtIndex.get(substr)]) break;
+                counts[wordsAtIndex.get(substr)]++;
+                j=j+wLen;
+            }
+            if(j==i+substrLen) res.add(i);
+        }
+        
+        return res;
+    }
+	
+	
+	// Approach #2:
+	public static List<Integer> findConcatenatedSubstrings_binarysearch(String s, String[] words) {
+        Arrays.sort(words);
+        
+        int wordsLen=words.length;
+        int wLen=words[0].length();
+        int substrLen=wordsLen*wLen;
+        
+        int[] wordsCount=new int[wordsLen];
+        int uniqueWords=0;
+        for(int i=0; i<wordsLen; i++){
+            if(words[i].compareTo(words[uniqueWords])==0) {
+                wordsCount[uniqueWords]=wordsCount[uniqueWords]+1;
+            }else {
+                uniqueWords=uniqueWords+1;
+                words[uniqueWords]=words[i];
+                wordsCount[uniqueWords]=wordsCount[uniqueWords]+1;
+            }
+        }
+        
+        int[] pickedWords=new int[uniqueWords+1];
+        
+        Queue<Integer> picked=new LinkedList<Integer>();
+        List<Integer> res=new ArrayList<Integer>();
+        String substr;
+        int foundIndex;
+        int j;
+        
+        for(int i=0; i+substrLen<=s.length(); i++){
+            j=i;
+            while(j<=i+substrLen-wLen){
+                substr=s.substring(j, j+wLen); 
+                // System.out.println(substr);
+                foundIndex=isFound(substr, words, 0, uniqueWords);
+                if(foundIndex>-1 && pickedWords[foundIndex]<wordsCount[foundIndex]) {
+                    pickedWords[foundIndex]=pickedWords[foundIndex]+1;
+                    if(pickedWords[foundIndex]==1) picked.add(foundIndex);
+                }else break;
+                j=j+wLen;
+            }
+            if(j==i+substrLen) res.add(i);
+            while(!picked.isEmpty()) pickedWords[picked.poll()]=0;
+        }   
+        
+        return res;
+    }
+    
+    private static int isFound(String str, String[] words, int l, int r){
+        int m;
+        String mStr;
+        while(l<=r){
+            m=l+(r-l)/2;
+            mStr=words[m];
+            if(str.compareTo(mStr)==0) return m;
+            else if(str.compareTo(mStr)<0) r=m-1;
+            else l=m+1;
+        }
+        return -1;
+    }
+	
+	// Approach #1: findConcatenatedSubstrings_triesearch()
+	// 
+	
+	public static List<Integer> findConcatenatedSubstrings_triesearch(String s, String[] words) {
+        // trie approach
+        int wLen=words[0].length();
+        int wordsLen=words.length;
+        int substrLen=wLen*wordsLen;
+        
+        MyTrie startOfWords=new MyTrie();
+        MyTrie parentTrie;
+        char character;
+        
+        for(String word: words){
+            parentTrie=startOfWords;
+            for(int c=0; c<wLen; c++){
+                character=word.charAt(c);
+                if(!parentTrie.nextTrie.containsKey(character)) parentTrie.nextTrie.put(character, new MyTrie());
+                parentTrie=parentTrie.nextTrie.get(character);
+            }
+            parentTrie.count++;
+            parentTrie.isEnd=true;
+        }
+        
+        List<Integer> res=new ArrayList<Integer>();
+        Queue<MyTrie> endOfVisited=new LinkedList<MyTrie>();
+        int j;
+        
+        for(int i=0; i<=s.length()-substrLen; i++){
+            parentTrie=startOfWords;
+            j=i;
+            // System.out.println(j);
+            while(j<i+substrLen){
+                character=s.charAt(j);
+                if(parentTrie.isEnd) {
+                    if(parentTrie.countVisits>=parentTrie.count) break;
+                    parentTrie.countVisits++;
+                    if(parentTrie.countVisits==1) endOfVisited.add(parentTrie);
+                    parentTrie=startOfWords;
+                }
+                if(!parentTrie.nextTrie.containsKey(character)) break;
+                parentTrie=parentTrie.nextTrie.get(character);
+                j++;
+            }
+            if(j==i+substrLen && parentTrie.countVisits<parentTrie.count) res.add(i);
+            while(!endOfVisited.isEmpty()) endOfVisited.poll().countVisits=0;
+        }
+        
+        return res;
+    }
+	
 	// ---------------------------------------------------------
 	// Using American keyboard
 	// Return the words from array words that can be typed using only 1 of the 3 rows on the keyboard
@@ -110,4 +274,18 @@ public class ArrayOfStringOps {
         
         return res;
     } 
+}
+
+//helper class for findConcatenatedSubstrings_trie()
+class MyTrie{
+	 HashMap<Character, MyTrie> nextTrie;
+	 int countVisits;
+	 int count;
+	 boolean isEnd;
+	 public MyTrie(){
+	     this.nextTrie=new HashMap<Character, MyTrie>();
+	     this.countVisits=0;
+	     this.count=0;
+	     this.isEnd=false;
+	 }
 }
